acce8ba3d57d95a14c25e07353ae374b0541d83f
shivanisingh73 <shivanis0703@gmail.com>
2019-02-21 16:05:26 +0530

    Add files via upload

---
+++ dayDate.js
@@ -0,0 +1,31 @@
+function clock()
+{
+        console.clear();
+        date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = ' '+days[day]+' '+d+'-'+months[month]+'-'+year+' '+h+':'+m+':'+s;
+        console.log(result);
+        setTimeout('clock();','1000');
+        return true;
+}
+clock();
